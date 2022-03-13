7ffd6910902f23cb4805a763394530ea5c697efa
RiveraDarwin <79887367+RiveraDarwin@users.noreply.github.com>
2022-03-13 16:19:42 -0700

    Add files via upload
    initial commit

---
+++ treasureHunt.js
@@ -0,0 +1,42 @@
+alert("Treasurehunt")
+
+const treasure = (location) => {
+    if(location === 'one') {
+      document.getElementById(location).innerHTML = '🎯'
+    } else if(location === 'two') {
+      document.getElementById(location).innerHTML = '💣'
+    } else if(location === 'three') {
+      document.getElementById(location).innerHTML = '💣'
+    } else if(location === 'four'){
+        document.getElementById(location).innerHTML= '💰'
+    } else if(location === 'five') {
+    document.getElementById(location).innerHTML = '💣'
+    } else if(location === 'six') {
+    document.getElementById(location).innerHTML = '💣'
+    } else if(location === 'seven'){
+      document.getElementById(location).innerHTML= '💰'
+    } else if(location === 'eight') {
+        document.getElementById(location).innerHTML = '💣'
+    } else if(location === 'nine') {
+        document.getElementById(location).innerHTML = '💣'
+    } else if(location === 'ten'){
+        document.getElementById(location).innerHTML= '💰'
+    }
+    else if(location === 'eleven'){
+        document.getElementById(location).innerHTML= '💰'
+    }else if(location === 'twelve'){
+        document.getElementById(location).innerHTML= '💰'
+    }
+    else if(location === 'thirteen'){
+        document.getElementById(location).innerHTML= '💰'
+    }
+    else if(location === 'fourteen'){
+        document.getElementById(location).innerHTML= '💰'
+    }
+    else if(location === 'fifteen'){
+        document.getElementById(location).innerHTML= '💰'
+    } 
+    else if(location === 'sixteen'){
+            document.getElementById(location).innerHTML= '💰'
+    }
+}
